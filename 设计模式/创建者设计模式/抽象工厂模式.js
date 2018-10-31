ac4f750f4968415b2e7b1c8410d82751575bfb93
riverwangjiang <11760966232qq.com>
2018-10-31 16:39:31 +0800

    '写完抽象工厂模式'

--- 设计模式/创建者设计模式/抽象工厂模式.js
+++ 设计模式/创建者设计模式/抽象工厂模式.js
@@ -9,6 +9,34 @@
 var VehicleFactory = function(subType,superType){
     if(typeof VehicleFactory[superType] === 'function'){
         //缓存类
-        function F(){}
+        function F(){};
+        F.prototype = new VehicleFactory[superType]();
+        //将子类constructor 指向子类
+        subType.constructor = subType;
+        //子类原型继承“父类”
+        subType.prototype = new F();
+    }else{
+        throw new Error('为创建该抽象类')
     }
 }
+// 小汽车抽象类
+VehicleFactory.Car = function(){
+    this.type = 'car';
+};
+VehicleFactory.Car.prototype = {
+    getPrice: function(){
+        return new Error('抽象类方法不能掉用')
+    }
+}
+// 使用
+// 宝马汽车子类
+var BMW = function(price,speed){
+    this.price = perice;
+    this.speed = speed;
+}
+//抽象工厂实现对Car抽象类的继承
+VehicleFactory(BMW,'Car');
+BMW.prototype.getPrice = function(){
+    return this.price;
+}
+
